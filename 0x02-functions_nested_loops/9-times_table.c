5dce5f9bf6824b2f2a67650f0fbaf145206bd2d6
YvonneOyin <olufemioyindamola37@gmail.com>
2022-10-19 04:52:57 -0700

    Learn your times table edit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,27 +15,21 @@ void times_table(void)
 
 	for (number = 0; number < 10; ++number)
 	{
+		_putchar('0');
 		for (multi = 1; multi < 10; ++multi)
 		{
+			_putchar(',');
+			_putchar(' ');
+
 			prod = multi * number;
-			if (multi == 0)
-			{
-				_putchar(prod + '0');
-			}
 
-			if (prod < 10 && multi != 0)
+			if (prod < 10)
 			{
 				_putchar(' ');
 				_putchar(prod + '0');
 			}
 
-			else if (prod <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			else
+			else if (prod >= 10)
 			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
