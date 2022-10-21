25d6738411dcc6fa3777a6d9d2d45339edadccae
YvonneOyin <olufemioyindamola37@gmail.com>
2022-10-20 20:19:26 -0700

    Donot print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 apart from 2 and 4 and you can
+ * only use _putchar twice
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		if (number != 2 && number != 4)
+		{
+			_putchar(number + 48);
+		}
+	}
+	_putchar('\n');
+}
