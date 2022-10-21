c90c4163e97ba709f348bfa4aac18e40aa0d45eb
YvonneOyin <olufemioyindamola37@gmail.com>
2022-10-20 20:56:12 -0700

    riint lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * print_line - print a straight line
+ *
+ * @n: is the number of times the _ character should be printed
+ */
+
+void print_line(int n)
+{
+	int lineCh;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (lineCh = 1; lineCh <= n; lineCh++)
+		{
+			_putchar('_-);
+		}
+		_putchar('\n');
+	}
+}
